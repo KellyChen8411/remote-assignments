7747e800d7f7fd3f93962de32c9e693d054fd405
Kelly Chen <kellychen841106@gmail.com>
2022-03-30 20:59:19 +0800

    Binary search second version

--- Week-2/Assignment-5/Assignment5.js
+++ Week-2/Assignment-5/Assignment5.js
@@ -1,52 +1,31 @@
-function binarySearchPosition(arry, target){
-    let minIndex;
-    let maxIndex;
+function binarySearchPosition(numbers, target){
+    let minIndex = 0;
+    let maxIndex = numbers.length - 1;
     let guessIndex;
     let targetIndex;
 
-    minIndex = 0;
-    maxIndex = arry.length - 1;
+    // ------------Full array is reasonable guesses range------------
 
-    // ------------Check if target is first or last element in array------------
-    if(arry[minIndex] === target){
-        targetIndex = minIndex;
-        return targetIndex;
-    } else if(arry[maxIndex] === target){
-        targetIndex = maxIndex;
-        return targetIndex;
-    }
-    // ------------Binary Search: now full array is reasonable guesses range------------
-
-    while(true){
+    while(minIndex <= maxIndex){
 
         // ------------Assign guessIndex as middle index of reasonable guesses range------------
         guessIndex = Math.round((minIndex+maxIndex)/2);
         // ------------Check if element of guessIndex is target, if not: reset reasonable guesses range------------
-        if(arry[guessIndex] === target){
+        if(numbers[guessIndex] === target){
             targetIndex = guessIndex;
             return targetIndex;
-        } else if(arry[guessIndex] > target){
-            maxIndex = guessIndex;
+        } else if(numbers[guessIndex] > target){
+            maxIndex = guessIndex - 1;
         } else {
-            minIndex = guessIndex;
+            minIndex = guessIndex + 1;
         }
 
     }
-    // // ------------Assign guessIndex as middle index of reasonable guesses range------------
-    // guessIndex = Math.round((minIndex+maxIndex)/2);
-    // // ------------Check if element of guessIndex is target, if not: reset reasonable guesses range------------
-    // if(arry[guessIndex] === target){
-    //     targetIndex = guessIndex;
-    //     return targetIndex;
-    // } else if(arry[guessIndex] > target){
-    //     maxIndex = guessIndex;
-    // } else {
-    //     minIndex = guessIndex;
-    // }
 
+    return -1; //target is not in array
 
 }
 
-// binarySearchPosition([1, 2, 5, 6, 7], 1)
-console.log( binarySearchPosition([1, 2, 5, 6, 7], 6) );
+console.log( binarySearchPosition([1, 2, 5, 6, 7], 1) ); // should print 0
+console.log( binarySearchPosition([1, 2, 5, 6, 7], 6) ); // should print 3
 
