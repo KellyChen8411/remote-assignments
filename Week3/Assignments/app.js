c38c3f94a3cbd8f421c9bf49013c08a85d89c573
Kelly Chen <kellychen841106@gmail.com>
2022-04-06 16:13:29 +0800

    Week3 Assignment 1

---
+++ Week3/Assignments/app.js
@@ -0,0 +1,14 @@
+// ------------Applicaiton initial setting-------------
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res)=>{
+    res.render('index');
+})
+
+app.listen(port, () => {
+    console.log(`Applicaiton is operating and listening on port ${port}`)
+  })
