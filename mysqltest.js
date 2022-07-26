6b21b91fa96adb249a6df861075da622192e483c
Kelly Chen <kellychen841106@gmail.com>
2022-07-26 21:53:08 +0800

    [Feature] Github action test version3

--- mysqltest.js
+++ mysqltest.js
@@ -1,16 +1,18 @@
-const mysql = require("mysql2/promise");
+// const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: "mysqlservice",
-  user: "root",
-  password: "12345678",
-  database: "nice_stay_test",
-});
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "mysqlservice",
+//   user: "root",
+//   password: "12345678",
+//   database: "nice_stay_test",
+// });
 
-async function testmysql(){
-    const house = await pool.query("SELECT * from house");
-    console.log(house);
-}
+// async function testmysql(){
+//     const house = await pool.query("SELECT * from house");
+//     console.log(house);
+// }
 
-testmysql();
+// testmysql();
+
+console.log('test');
